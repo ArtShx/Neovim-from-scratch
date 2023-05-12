0b0ad859318ad284264e1af4379864ddb76abc61
Arthur Miada <artshx@gmail.com>
2023-05-12 09:39:13 -0300

    Applied my custom mods

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -25,6 +25,11 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+-- Normal GUI Editor Shortcuts
+keymap("n", "<C-w>", ":Bdelete<CR>", opts)
+keymap("n", "<C-s>", ":w<CR>", opts)
+keymap("i", "<C-s>", "<ESC>:w<CR>", opts)
+
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
